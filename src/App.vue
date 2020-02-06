979aab03fdf39ea067cebd12761e0ad751e937df
Adam Fenemore <awfenemore@gmail.com>
2020-02-06 21:09:22 +1300

    header nearly finished, pretty responsive I guess

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,16 @@
 <template>
   <v-app>
+    <Header v-on:openProjects="projClick" v-on:openStories="storiesClick" />
 
+    <div :style="{ height: this.menuProj + 'px' }" class="menu-to-open">
+      Proj
+    </div>
 
-    <v-app-bar flat color="#3d3d3d" max-height="100">    <!-- to make smaller on mobile-->
-      <v-app-bar-nav-icon class="d-sm-none"></v-app-bar-nav-icon>
-      <v-app-bar-nav-icon class="d-none d-md-flex ml-3 mt-3">
-        <router-link to="/" tag="span" style="cursor: pointer">
-          <img src="./assets/pli-grey-logo.svg" alt="Pli Logo" height="60" width="80" />
-        </router-link>
-      </v-app-bar-nav-icon>
-      <v-toolbar-title class="ml-12 mt-8 ">Design, Stories, Improvement</v-toolbar-title>
-    </v-app-bar>
+    <div :style="{ height: this.menuStories + 'px' }" class="menu-to-open">
+      Stories
+    </div>
 
     <div class="top-border"></div>
-
-
-    <!-- <v-navigation-drawer v-model="burgerOpen" app>
-      <v-list>
-        <v-list-tile v-for="item in menuItems" :key="item.title" :to="item.path">
-          <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>{{ item.title }}</v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-toolbar app max-height="100">
-      <span class="hidden-sm-and-up">
-        <v-toolbar-side-icon @click="burgerOpen = !burgerOpen">
-        </v-toolbar-side-icon>
-      </span>
-
-      <v-toolbar-title class="hidden-xs-only">
-        <router-link to="/" tag="span" style="cursor: pointer">
-          <img src="./assets/pli-grey-logo.svg" alt="Pli Logo" height="60" width="80" /> 
-        </router-link>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-
-      <v-toolbar-items class="hidden-xs-only">
-        <v-btn
-          flat
-          v-for="item in menuItems"
-          :key="item.title"
-          :to="item.path">
-          <v-icon left dark>{{ item.icon }}</v-icon>
-          {{ item.title }}
-        </v-btn>
-      </v-toolbar-items>
-    </v-toolbar> -->
-
-
 
     <v-content>
       <router-view></router-view>
@@ -59,49 +18,38 @@
 
     <div class="bot-border"></div>
 
-    <v-footer color="#3d3d3d" min-height="80">
-      <v-row>
-        <v-col cols="3" md="1">
-          <img src="./assets/pli-grey-logo.svg" alt="Pli Logo" height="60" width="80" />
-        </v-col>
-
-        <v-col cols="9" md="5" class="footer-left">   
-          Pli is a website about design, interesting stories, and doing things a little better. Thanks for visiting.
-        </v-col>
-
-        <v-col cols="12"  md="4" class="footer-mid">
-            <router-link to="/">Home</router-link> |
-            <router-link to="aboutpli">About Pli</router-link> |
-            <router-link to="aboutme">About Me</router-link> |
-            <router-link to="albumofthenow">Album of the Now</router-link> |
-            <router-link to="aboutpli">Back to Top</router-link>
-            <!-- TODO send this to the top of the page, not another link -->
-        </v-col>
-
-        <v-col cols="12"  md="2" class="footer-right">
-          A Fenemore | {{ new Date().getFullYear() }}
-        </v-col>
-      </v-row>
-    </v-footer>
+    <Footer />
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import Footer from "./components/Footer.vue";
+import Header from "./components/Header.vue";
 
 export default Vue.extend({
   name: "App",
+  components: {
+    Footer,
+    Header
+  },
   data() {
     return {
-      burgerOpen: false,
-      menuItems: [
-        { title: "Home", path: "/home", icon: "home" },
-        { title: "Sign Up", path: "/signup", icon: "face" },
-        { title: "Sign In", path: "/signin", icon: "lock_open" }
-      ]
+      menuProj: 0,
+      menuStories: 0
     };
+  },
+  methods: {
+    projClick: function() {
+      this.menuProj = 100;
+      // Working here. If one is already open then close it, and make them toggle to closed if open
+      // Change the button stylings on menu open
+    },
+    storiesClick: function() {
+      this.menuStories = 100;
+    }
   }
 });
 </script>
 
-<style lang="scss" src="./styles/base.scss"></style>
+<style lang="scss" src="./styles/App.scss"></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-app-bar flat color="#3d3d3d">
+      <!-- to make smaller on mobile-->
+      <v-row>
+        <v-col cols="3" md="1" class="main-logo pt-12 pl-2 pl-md-4">
+            <router-link to="/" tag="span" style="cursor: pointer">
+              <img class="ml-md-3 ml-0 mt-md-6 mt-7 d-flex" src="../assets/pli-grey-logo.svg" alt="Pli Logo" height="60" width="80"/>
+            </router-link>
+        </v-col>
+
+        <v-col cols="0" md="3" class="d-none d-md-flex pt-md-12 mt-md-3">
+          <v-toolbar-title class="ml-4 mt-8">Design, Stories, Improvement</v-toolbar-title>
+        </v-col>
+
+        <v-col cols="9" md="6" class="px-8 pt-8 pt-md-12 pl-md-8">
+            <v-row class="py-4 px-md-0 menu-container">
+                <v-col class="d-none d-md-flex" cols="" md="1"></v-col>
+                <v-col class="menu-col" cols="4" md="2"><a v-on:click="$emit('openProjects')"><PliMenuButton buttonText="Projects" class="menu-button-left menu-projects"/></a></v-col>
+                <v-col class="menu-col" cols="4" md="2"><a v-on:click="$emit('openStories')"><PliMenuButton buttonText="Stories" class="menu-button-left menu-stories"/></a></v-col>
+                <v-col class="menu-col" cols="4" md="2"><router-link to="albumofthenow"><PliMenuButton buttonText="Albums" class="menu-button-center menu-albums"/></router-link></v-col>
+                <v-col class="d-md-none" cols="2" md="0"></v-col>
+                <v-col class="menu-col" cols="4" md="2"><router-link to="aboutpli" ><PliMenuButton buttonText="About Pli" class="menu-button-right menu-pli"/></router-link></v-col>
+                <v-col class="menu-col" cols="4" md="2"><router-link to="aboutme" ><PliMenuButton buttonText="About Me" class="menu-button-right menu-me"/></router-link></v-col>
+                <v-col class="d-md-none" cols="2" md="0"></v-col>
+                <v-col class="d-none d-md-flex" cols="" md="1"></v-col>
+            </v-row>
+        </v-col>
+
+        <v-col cols="0" md="2" class="d-none d-md-flex">
+            <!-- Album of the now placeholder -->
+            <div class="d-none d-md-flex album-otn"></div>
+        </v-col>
+
+      </v-row>
+
+    </v-app-bar>
+</template>
+
+<script>
+import PliMenuButton from "./micro/PliMenuButtons.vue"
+
+export default {
+  name: "Header",
+  components : {
+      PliMenuButton
+  }, 
+  data() {
+    return {
+      menuOpen: 0
+    };
+  }
+};
+</script>
+
+<style lang="scss" src="../styles/Header.scss"></style>
