273ee6dc236feba62ce5123a9c7c2cdf226f511e
Adam Fenemore <awfenemore@gmail.com>
2020-04-11 15:59:22 +1200

    Changes to base for mobile and tablet

--- src/components/micro/Header.vue
+++ src/components/micro/Header.vue
@@ -5,7 +5,7 @@
       <v-col cols="3" md="1" class="main-logo pt-12 px-2 pl-md-4">
         <router-link to="/" tag="span" style="cursor: pointer">
           <img
-            class="ml-0 ml-md-3 mt-8 mt-md-6 d-flex"
+            class="ml-0 ml-md-3 mt-5 mt-md-6 d-flex"
             src="../../assets/pli-grey-logo.svg"
             alt="Pli Logo"
             height="60"
@@ -15,17 +15,19 @@
       </v-col>
 
       <v-col cols="0" md="3" class="d-none d-md-flex pt-md-12 mt-md-3">
-        <v-toolbar-title class="ml-4 mt-9"
+        <v-toolbar-title
+          class="ml-4 ml-md-8 ml-lg-4 mt-9 mt-md-0 mt-lg-9 header-title"
           >Design, Stories, Improvement</v-toolbar-title
         >
       </v-col>
 
-      <v-col cols="9" md="6" class="pl-0 pl-md-8 pr-2 pt-8 pt-md-12">
+      <v-col cols="9" md="8" lg="6" class="pl-0 pl-md-8 pr-2 pt-8 pt-md-12">
         <v-row class="pt-6 px-md-0 menu-container">
           <v-col
             class="d-none d-md-flex"
             cols=""
             md="1"
+            lg="1"
             style="padding: 0;"
           ></v-col>
           <v-col class="menu-col" cols="4" md="2" style="padding: 0;"
@@ -71,7 +73,7 @@
           <v-col
             class="d-none d-md-flex"
             cols=""
-            md="1"
+            lg="1"
             style="padding: 0;"
           ></v-col>
         </v-row>
@@ -79,7 +81,7 @@
 
       <v-col cols="0" md="2" class="d-none d-md-flex" style="padding: 0;">
         <!-- Album of the now placeholder -->
-        <div class="d-none d-md-flex album-otn mt-10 pt-3">
+        <div class="d-none d-lg-flex album-otn mt-10 pt-3">
           <div class="album-otn-left pt-2">
             <h4>Album of the Now</h4>
             <h5>{{ albumOfTheNow.title }} - {{ albumOfTheNow.artist }}</h5>
