0e76c649da687f0fbc1248ec3d7e03ddd4b0ec95
Adam Fenemore <adam.fenemore@intergen.co.nz>
2020-12-30 19:56:11 +1300

    Add some albums

--- src/data/albums.js
+++ src/data/albums.js
@@ -1,5 +1,23 @@
 const albums = [
   {
+    title: "This Empty Town",
+    artist: "Jaguar Sun",
+    release: "10/07/2020",
+    link: "https://jaguarsun.bandcamp.com/album/this-empty-town",
+    image: require("../assets/albumImages/jaguar-sun-this-empty-town.jpg"),
+    whyILike:
+      "Great little album that I feel is well slept on. Jaguar Sun music is excellent, very much soundscape based and just sounds great. Your recognition will come Mr Jaguar Sun, Sir."
+  },
+  {
+    title: "Sideways to New Italy",
+    artist: "Rolling Blackouts Coastal Fever",
+    release: "05/06/2020",
+    link: "https://rollingblackoutscoastalfever.bandcamp.com/album/sideways-to-new-italy",
+    image: require("../assets/albumImages/rcbf-sideways.jpg"),
+    whyILike:
+      "I think this is the album of 2020, its just full of good stuff. Its very Australian, guitar driven and mostly energetic. Highly recommend this album."
+  },
+  {
     title: "The Main Thing",
     artist: "Real Estate",
     release: "28/02/2020",
@@ -16,6 +34,42 @@ const albums = [
     image: require("../assets/albumImages/Tame_Impala_-_The_Slow_Rush.png"),
     whyILike:
       "I have loved Tame Impala since first hearing them in 2009, and although this isn't my favourite Tame album, I still enjoy it a lot. To me, Kevin Parker is just an interesting person who sees the world in an interesting way, and I am glad to get a glimpse of how he sees the world through his music."
+  },
+  {
+    title: "Jinx",
+    artist: "Crumb",
+    release: "14/06/2019",
+    link: "https://crumbtheband.bandcamp.com/album/jinx",
+    image: require("../assets/albumImages/crumb-jinx.jpg"),
+    whyILike:
+      "This is the album of 2019 for realsies. Start at the start and you won't skip a single song, it is fantastic."
+  },
+  {
+    title: "The Heights",
+    artist: "Far Caspian",
+    release: "18/06/2019",
+    link: "https://soundcloud.com/far-caspian/the-heights",
+    image: require("../assets/albumImages/far-caspain-the-heights.jpg"),
+    whyILike:
+      "The first Far Caspian EP, Between Days, is probably the best 20 minute collection of music from 2018, and this is more of the good stuff."
+  },
+  {
+    title: "Oncle Jazz",
+    artist: "Men I Trust",
+    release: "13/09/2019",
+    link: "https://menitrust.bandcamp.com/album/oncle-jazz",
+    image: require("../assets/albumImages/men-i-trust-oncle-jazz.jpg"),
+    whyILike:
+      "Sit back, relax, and go on a nice journey, with some really talented people. Men I Trust write great music, and this is a long play full of great stuff."
+  },
+  {
+    title: "Deciever",
+    artist: "DIIV",
+    release: "04/10/2019",
+    link: "https://diiv.bandcamp.com/album/deceiver",
+    image: require("../assets/albumImages/diiv-deceiver.jpg"),
+    whyILike:
+      "So DIIV have a huge amount of great stuff, but boy this album really does it for me. There are some very powerful songs here, and if you don't listen to Acheron on nearly full volume, you might be doing it wrong."
   }
 ];
 
